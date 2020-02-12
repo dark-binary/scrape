4ec6928c9c692c6a94c7ede889e428d876477a8e
darkbinary <darkbinary@localhost.localdomain>
2020-02-13 04:07:31 +1300

    formatting

--- scrape.sh
+++ scrape.sh
@@ -1,22 +1,24 @@
 #!bin/bash/
-echo "Total number of users: "
+echo -e "-----------------------------------------"
+
 number_of_users=$(grep '100.*[^:]' /etc/group | wc -l)
-echo $number_of_users
+echo "Total number of users: $number_of_users"
 echo -e "\n"
 
-echo -e "List of all users: "
+
 users=$(grep '100.*[^:]' /etc/group | cut -d: -f1)
+echo -e "List of all users: "
 echo -e $users
 echo -e "\n"
 
-echo -e "Number of admin users: "
+
 number_of_admin=$(grep sudo /etc/group | cut -d: -f4 | tr ',' '\n' | wc -l)
-echo $number_of_admin
+echo -e "Number of admin users: $number_of_admin"
 echo -e "\n"
 
-echo -e "Number of non admin users: "
+
 let "number_of_non_admin=$number_of_users-$number_of_admin"
-echo $number_of_non_admin
+echo -e "Number of non admin users: $number_of_non_admin"
 echo -e "\n"
 
 echo -e "List of admin users: "
@@ -26,4 +28,5 @@ echo -e "\n"
 
 echo -e "List of non admin users: "
 comm -23 <(tr ' ' $'\n' <<< $users | sort) <(tr ' ' $'\n' <<< $admin_users | sort)
-echo -e "\n"
+
+echo -e "-----------------------------------------"
